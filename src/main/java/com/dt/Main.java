e642c48d5931a476fa0fb41a99c9d2afbb0e2c20
Roman <126401798+DefRoga@users.noreply.github.com>
2023-10-04 15:30:02 +0500

    Update Main.java

--- src/main/java/com/dt/Main.java
+++ src/main/java/com/dt/Main.java
@@ -1,7 +1,10 @@
 package com.dt;
 
 public class Main {
+
+    private static String msg = "Hello QA.guru!!!!!";
+    
     public static void main(String[] args) {
-        System.out.println("Hello QA.guru!");
+        System.out.println(msg);
     }
 }
